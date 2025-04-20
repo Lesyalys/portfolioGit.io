4be56ee4402c0d3a3016fd4f81911bee50f95106
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-20 12:56:52 +0500

    Updates

--- assets/index-D9OBj9m1.js
+++ assets/index-D9OBj9m1.js
@@ -1,9 +1,9 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const t of i.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&o(t)}).observe(document,{childList:!0,subtree:!0});function n(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(e){if(e.ep)return;e.ep=!0;const i=n(e);fetch(e.href,i)}})();function d(s){s.innerHTML=`
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(e){if(e.ep)return;e.ep=!0;const i=o(e);fetch(e.href,i)}})();function c(){const t=document.getElementById("mode"),r=document.getElementById("logo");if(t){const o=t.getAttribute("href");o==="/portfolioGit.io/src/style.css"?(console.log("1"),t.setAttribute("href","/portfolioGit.io/src/light.css"),r.setAttribute("src","./img/Frame2.png")):o==="/portfolioGit.io/src/light.css"&&(console.log("2"),t.setAttribute("href","/portfolioGit.io/src/style.css"),console.log(t),r.setAttribute("src","./img/Frame.png"),console.log(r))}}function d(t){t.innerHTML=`
     <div class="screen">
         <div id = "rightBlock">
     
           <div id="meDark">  
-            <img src="./img/Frame.png">
+            <img id="logo" src="./img/Frame.png">
           </div>
     
           <div id="info">
@@ -54,4 +54,4 @@
         </div>
       </div>
       <footer>fdsfsd</footer>
-    `,document.getElementById("meDark").addEventListener("click",()=>console.log("hi"))}d(document.querySelector("#app"));
+    `,document.getElementById("meDark").addEventListener("click",()=>c())}d(document.querySelector("#app"));
