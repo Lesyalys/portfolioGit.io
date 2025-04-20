0212e48c9993e357dcffa668a54436adfffdd1ba
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-20 13:26:10 +0500

    new

--- src/modeShem.js
+++ src/modeShem.js
@@ -5,16 +5,12 @@ export function shem(){
         if (mode){
             const style = mode.getAttribute('href');
             if (style === '/portfolioGit.io/src/style.css'){
-                console.log('1')
                 mode.setAttribute('href','/portfolioGit.io/src/light.css');
                 logo.setAttribute('src','./img/Frame2.png');
             }
             else if (style === '/portfolioGit.io/src/light.css'){
-                console.log('2');
                 mode.setAttribute('href','/portfolioGit.io/src/style.css');
-                console.log(mode);
                 logo.setAttribute('src','./img/Frame.png');
-                console.log(logo);
             }
         }
 }
