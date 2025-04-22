8f6d5bf1d97089d895dd3001cac0b2944c93e277
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-22 09:40:57 +0500

    new

--- src/modeShem.js
+++ src/modeShem.js
@@ -1,18 +1,28 @@
 export function shem(){
-        const mode = document.getElementById('mode');
+    document.body.classList.toggle('dark-shem');
+        // const mode = document.getElementById('mode');
         const logo = document.getElementById('logo');
+        const isDark = document.body.classList.contains('dark-shem');
 
-        if (mode){
-            const style = mode.getAttribute('href');
-            if (style === './src/style.css'){
-                console.log('dark')
-                mode.setAttribute('href','./src/light.css');
-                logo.setAttribute('src','./img/Frame2.png');
-            }
-            else if (style === './src/light.css'){
-                console.log('light')
-                mode.setAttribute('href','./src/style.css');
-                logo.setAttribute('src','./img/Frame.png');
-            }
+        logo.src = isDark ? '/img/Frame2.png' : '/img/Frame.png';
+        localStorage.setItem('theme',isDark ? 'dark' : 'light');
+
+        const saveTheme = localStorage.getItem('theme');
+        if (saveTheme === 'dark'){
+            document.body.classList.add('dark-shem')
         }
+
+        // if (mode){
+        //     const style = mode.getAttribute('href');
+        //     if (style === './src/style.css'){
+        //         console.log('dark')
+        //         mode.setAttribute('href','./src/light.css');
+        //         logo.setAttribute('src','./img/Frame2.png');
+        //     }
+        //     else if (style === './src/light.css'){
+        //         console.log('light')
+        //         mode.setAttribute('href','./src/style.css');
+        //         logo.setAttribute('src','./img/Frame.png');
+        //     }
+        // }
 }
