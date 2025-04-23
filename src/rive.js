3b614824fe9b51c196b22c473051189c67bffb06
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-04-23 15:47:55 +0500

    new

--- src/rive.js
+++ src/rive.js
@@ -15,50 +15,14 @@ import { Rive, StateMachineInputType } from "@rive-app/canvas";
         const triggere = input.find(i => i.name === 'Raining');
         riveCanvas.onclick = () => {
             if (triggere.value === false){
-                console.log(triggere.value);
                 triggere.value = true;
             }
             else if (triggere.value === true){
-                console.log(triggere.value);
                 triggere.value = false;
             }}
         
       }
     });
-
-    // const input = riveInstance.stateMachineInputs('weather');
-    // console.log(input)
-    
-    // input.forEach((i)=>{
-    //     const inpName = i.name;
-    //     const inpType = i.type;
-    //     switch(inpType) {
-    //         case riveCanvas.StateMachineInputType.Trigger:
-    //             i.fire();
-    //             break;
-    //         case riveCanvas.StateMachineInputType.Number:
-    //             i.value = 42;
-    //             break;
-    //         case riveCanvas.StateMachineInputType.Boolean:
-    //             i.value = true;
-    //             break;
-    //     }
-    //     }
-    // )
-
-
-    //ANIMATION
-    // riveCanvas.onclick = () => {
-    //     console.log('click');
-    //     if (riveInstance.playingAnimationNames.includes('windshield_wipers')){
-    //         riveInstance.pause('windshield_wipers');
-    //         riveInstance.play('broken');
-    //     }
-    //     else {
-    //         riveInstance.pause('broken');
-    //         riveInstance.play('windshield_wipers');
-    //     }
-    // }
 
   window.addEventListener(
     "resize",
